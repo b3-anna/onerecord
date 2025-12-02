0d133859c80478d066c88d77a1537f8fea59d124
b3-anna <beauty.browlash.ipli@gmail.com>
2025-12-02 14:54:24 +0100

    Add document scanning service with OpenCV.js

--- libs/data-access/src/index.ts
+++ libs/data-access/src/index.ts
@@ -1,5 +1,6 @@
 export * from './lib/services/one-record.service';
 export * from './lib/services/access-control.service';
 export * from './lib/services/mock-data.service';
+export * from './lib/services/document-scan.service';
 export * from './lib/models/logistics-object.model';
 export * from './lib/utils/json-ld.util';
